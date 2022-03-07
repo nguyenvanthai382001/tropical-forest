7e8eb218917280e9e95fe4f73d5ce6cffb764acd
ThaiNguyen <thainax101@gmail.com>
2022-03-07 20:57:13 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,43 @@ Show_list_item($(".news-page .col-md-8"), $(".news-page .news-page-item"), 3);
 //   $(".sub-menu").slideToggle();
 //   $(this).toggleClass("active");
 // });
+
+// thay đổi số lượng sản phẩm
+(function () {
+  window.inputNumber = function (el) {
+    var min = el.attr("min") || false;
+    var max = el.attr("max") || false;
+
+    var els = {};
+
+    els.dec = el.prev();
+    els.inc = el.next();
+
+    el.each(function () {
+      init($(this));
+    });
+
+    function init(el) {
+      els.dec.on("click", decrement);
+      els.inc.on("click", increment);
+
+      function decrement() {
+        var value = el[0].value;
+        value--;
+        if (!min || value >= min) {
+          el[0].value = value;
+        }
+      }
+
+      function increment() {
+        var value = el[0].value;
+        value++;
+        if (!max || value <= max) {
+          el[0].value = value++;
+        }
+      }
+    }
+  };
+})();
+
+inputNumber($(".count-input"));
